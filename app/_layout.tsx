908617b80c6052db1f371af2d76bc6c3dc6c1a6e
mtnrconcept <149347251+mtnrconcept@users.noreply.github.com>
2025-10-19 07:22:38 +0200

    Updated package.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,8 @@ export default function RootLayout() {
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="tournament/[id]" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
